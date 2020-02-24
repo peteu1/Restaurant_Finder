ead4b3cb8fec244a920fcbcb74630be2eb525850
Pete Umland <peteu@vt.edu>
2020-02-24 09:43:38 -0500

    Add skeleton structure of app

---
+++ Yelp_API.py
@@ -0,0 +1,4 @@
+import creds
+
+#creds.Client_ID
+#creds.API_Key
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    msg = "Welcome to the launch page for my restaurant finder."
-    # TODO: Add more information (move to text file)
+    # TODO: Add more information to home.html
     # TODO: Link to application
-    return msg
+    return render_template("home.html")
 
 @app.route("/restaurant_finder")
 def restaurant_finder():
     # TODO
-    return "This will be the app."
+    return render_template("app.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
