cf1382ada9d38d56579f4e398d0588c42dd5d857
peteu1 <peteu@vt.edu>
2020-03-06 17:16:41 -0500

    restore home launcher and major structure changes

--- home.py
+++ home.py
@@ -37,26 +37,29 @@ restaurants = Restaurants()
 storedData = StoredData()
 
 
-@app.route("/test", methods=['POST', 'GET'])
-def test_post():
-    print("Test method:", request.method)
-    if request.method == 'POST':
-        data = request.get_json()
-        print("Received:", data)
-        names = data["names"]
-        for name in names:
-            print("Param received:", name)
-        return render_template("home.html")
+# @app.route("/getLocation", methods=['POST', 'GET'])
+# def test_post():
+#     print("Test method:", request.method)
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         print("Received:", data)
+#         lat = data["lat"]
+#         lon = data["lon"]
+#         print("location received:", lat, lon)
+#         storedData.setLatLon(lat, lon)
+#         # Get results from API and load app.html
+#         # results, reviews = restaurants.reload_results()
+#         # storedData._template_rendered = True
+#         # return render_template("app.html", **storedData.collect_data(results, reviews))
+#         # TODO: Call background_process()
+#         return redirect(url_for("background_process")) # restaurant_finder
 
-    return render_template("home.html")
+#     return render_template("home.html")
 
 @app.route("/", methods=['POST', 'GET'])
 def restaurant_finder():
+    print(">restaurant_finder() called")
     return render_template("home.html")
-    # TODO: Restore after testing
-    # results, reviews = restaurants.reload_results()
-    # storedData._template_rendered = True
-    # return render_template("app.html", **storedData.collect_data(results, reviews))
 
 
 food_selection_map = {
@@ -69,11 +72,9 @@ food_selection_map = {
     "6": "Bar",
 }
 
-@app.route('/restaurant_finder')
+@app.route('/restaurant_finder', methods=['POST', 'GET'])
 def background_process():
-    if not storedData._template_rendered:
-        # If page is loaded for first time directly from a URL with parameters (rather than "/")
-        restaurants.reload_results()  # TODO: Need this? Make more efficient?
+    print("> background_process() called")
     args = request.args
     print("Data:", args)
     if storedData.location_received == "0":
@@ -81,22 +82,37 @@ def background_process():
         lat, lon = float(args["lat"]), float(args["lon"])
         storedData.setLatLon(lat, lon)
         results, reviews = restaurants.set_location(lat, lon)
+        storedData._template_rendered = True
         return render_template("app.html", **storedData.collect_data(results, reviews))
 
-    results, reviews = restaurants.update_excluded_prices(int(args['cheap']), int(args['pricey']))
-    # TODO: Implement option in html to view the reviews (max 3)
-    food_idx = args['sel_food_type']
-    if food_idx != "0":
-        food_name = food_selection_map[food_idx]
-        print("Food selection:", food_name)
-        results, reviews = restaurants.set_meal(food_name)
+    if request.method == 'POST':
+        data = request.get_json()
+        print("Received:", data)
+        # TODO: Properly track these and toggle values
+        if 'cheap' in data.keys():
+            print("Cheap clicked!")
+            cheap = 1
+            pricey = 0
+        if 'pricey' in data.keys():
+            print("Pricey clicked!")
+            cheap = 0
+            pricey = 1
+        results, reviews = restaurants.update_excluded_prices(cheap, pricey)
+    else:
+        results, reviews = restaurants.reload_results()
+    # food_idx = args['sel_food_type']
+    # if food_idx != "0":
+    #     food_name = food_selection_map[food_idx]
+    #     print("Food selection:", food_name)
+    #     results, reviews = restaurants.set_meal(food_name)
     
     print("New length:", len(results))
     print("Filter pricey:", restaurants.filter_pricey)
     print("Filter cheap:", restaurants.filter_cheap)
     print("All length", len(restaurants.all_results))
     
-    return render_template("app.html", **storedData.collect_data(results, reviews, food_idx))
+    return render_template("app.html", **storedData.collect_data(results, reviews))
+    #return render_template("app.html", **storedData.collect_data(results, reviews, food_idx))
     
 
 
