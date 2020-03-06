720df203d86fce866ff0dd0d937b744a02e36165
peteu1 <peteu@vt.edu>
2020-03-05 23:21:16 -0500

    Update plan

--- home.py
+++ home.py
@@ -91,4 +91,4 @@ def background_process():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
